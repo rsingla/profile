9210f9735823284b92d3a3fa1e7510efa59becaf
Rajeev Singla <rajeev1.singla@gmail.com>
2019-08-06 00:59:37 -0700

    Update app.py

--- app.py
+++ app.py
@@ -9,5 +9,10 @@ def hello_world():
     target = os.environ.get('TARGET', 'World')
     return 'Hello {}!\n'.format(target)
 
+@app.route('/file')
+def hello_world():
+    target = os.environ.get('TARGET', 'World')
+    return 'Hello {}!\n'.format(target)
+
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
